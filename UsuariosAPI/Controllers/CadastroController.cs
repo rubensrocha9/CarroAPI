92c538a48b7712e6a26071befcf113c183cc2e84
Rubens Rocha <rubensrocha50@gmail.com>
2022-09-01 23:02:43 -0300

    Usuario Cadastro

--- UsuariosAPI/Controllers/CadastroController.cs
+++ UsuariosAPI/Controllers/CadastroController.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Services;
 
 namespace UsuariosAPI.Controllers
 {
-    [Route("[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class CadastroController : ControllerBase
     {
+        private CadastroService _cadastroService;
 
+        public CadastroController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
 
         [HttpPost]
-        public IActionResult CadastroUsuario(CreateUsuarioDto createDto)
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
         {
+            Result resultado = _cadastroService.CadastraUsuario(createDto);
+            if (resultado.IsFailed)
+            {
+                return StatusCode(500);
+            }
             return Ok();
         }
     }
--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UsuariosAPI.Data;
+using UsuariosAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<UserDbContext>(op => op.UseSqlServer(connection));
 builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
     .AddEntityFrameworkStores<UserDbContext>();
 
+builder.Services.AddScoped<CadastroService, CadastroService>();
+        
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
